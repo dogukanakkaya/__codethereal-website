fb2b2257849d297d2093247ed52108095d2a8fc1
dogukan <doguakkaya27@hotmail.com>
2021-02-07 21:13:51 +0300

    maintenance mode page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,12 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Codethereal</title>
-    <link rel="stylesheet" href="{{ asset('site/maintenance.css') }}">
+    <link rel="stylesheet" href="{{ asset('site/css/maintenance.css') }}">
 </head>
 <body>
-    <div class="maintenance" style="background-image: url('https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')">
-        <div class="box">
-
+    <div class="maintenance" style="background: linear-gradient(to right,rgba(12, 41, 116, 0.84) 0%,rgba(35, 107, 237, 0.84) 48%);">
+        <div class="content">
+            <h1>Codethereal</h1>
+            <p>Our web site is in maintenance mode, please visit again later.</p>
         </div>
     </div>
 </body>
