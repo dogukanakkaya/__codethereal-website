38d611d20a8b35e1457862a5b11160e64603c909
root <root@srv.donanimevi.com>
2021-02-10 19:28:53 +0300

    .gitignore modified

---
+++ config/site.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'home_top' => 1,
+    'categories' => 2,
+    'home_parallax' => 7,
+    'header_menu' => 3,
+    'category_links' => 4,
+    'quick_links' => 5,
+    'cards' => 0,
+    'footer' => 11,
+    'setting_names' => [
+        'logo_light',
+        'logo_dark',
+        'title',
+        'description',
+        'linkedin',
+        'github',
+        'phone',
+        'email',
+        'address'
+    ],
+    'locale_names' => [
+        'en' => 'English',
+        'tr' => 'Turkish'
+    ],
+
+    'browser' => null
+]; 
