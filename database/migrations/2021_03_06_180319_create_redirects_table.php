d5da806fbe09f4073ac586acf9646d101c48d538
root <root@srv.donanimevi.com>
2021-03-06 18:24:50 +0300

    added redirects table migration

---
+++ database/migrations/2021_03_06_180319_create_redirects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRedirectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('redirects', function (Blueprint $table) {
+            $table->id();
+            $table->string('from');
+            $table->string('to');
+            $table->integer('code');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('redirects');
+    }
+}
